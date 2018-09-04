8f7d980985fccd80e325731c56810d53d83c7085
Ardavast Dayleryan <ardavast@noiseoverip.com>
2018-09-04 23:49:29 +0300

    Add Python: Debugging

---
+++ python/17_debugging/p01.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Standardize Mobile Number Using Decorators
+https://www.hackerrank.com/challenges/standardize-mobile-number-using-decorators
+"""
+
+
+def is_vowel(letter):
+    return letter in 'aeiouy'
+
+
+def score_words(words):
+    score = 0
+    for word in words:
+        num_vowels = 0
+        for letter in word:
+            if is_vowel(letter):
+                num_vowels += 1
+        if num_vowels % 2 == 0:
+            score += 2
+        else:
+            score += 1
+    return score
+
+
+if __name__ == '__main__':
+    n = int(input())
+    words = input().split()
+    print(score_words(words))
